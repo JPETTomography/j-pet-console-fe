8cc248433b5c3b544f5c74c4b13de4516a19d1aa
Jakub Perlak <j.perlak@cyfronet.pl>
2024-10-05 22:04:40 +0200

    Improve NotFound page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,8 +1,10 @@
 const NotFound = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
       <h1>NOT FOUND</h1>
-      <a href="/">Back to main page</a>
+      <a href="/" className="text-sky-700 hover:text-sky-900 hover:underline">
+        Back to main page
+      </a>
     </div>
   );
 };
