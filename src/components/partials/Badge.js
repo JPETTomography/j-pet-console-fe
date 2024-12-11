dc4b0db96a3ae3b8da6212092a73c84e949d3446
Jakub Perlak <jakub.k.perlak@interia.pl>
2024-12-11 12:29:24 +0100

    Add missing badges variants

--- src/components/partials/Badge.js
+++ src/components/partials/Badge.js
@@ -6,8 +6,20 @@ function badgeVariantStyling(variant) {
       return "bg-sky-200 border-sky-300 text-sky-800";
     case "closed":
       return "bg-slate-600 border-slate-600 text-white";
+    case "online":
+      return "bg-green-100 border-green-500 text-green-700";
+    case "offline":
+      return "bg-gray-100 border-gray-500 text-gray-700";
+    case "damaged":
+      return "bg-red-100 border-red-500 text-red-700";
+    case "in-repair":
+      return "bg-yellow-100 border-yellow-500 text-yellow-700";
+    case "commissioned":
+      return "bg-blue-100 border-blue-500 text-blue-700";
+    case "decommissioned":
+      return "bg-purple-100 border-purple-500 text-purple-700";
     default:
-      return "bg-white border-slate-400";
+      return "bg-white border-slate-600";
   }
 }
 
