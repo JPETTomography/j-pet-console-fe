7adb706e339f1768102725a70360b6269a048941
Jakub Perlak <jakub.k.perlak@interia.pl>
2024-12-18 12:05:17 +0100

    Add CurrentUser logic for header

--- src/components/partials/Nav.js
+++ src/components/partials/Nav.js
@@ -1,29 +1,42 @@
 import { NavLink } from "react-router-dom";
 
-const Nav = () => {
+function navFoRole(role) {
+  switch (role) {
+    case "shifter":
+      return ["experiments", "tags", "radioisotopes"];
+    case "coordinator":
+      return ["experiments", "detectors", "tags", "radioisotopes"];
+    case "admin":
+      return ["users", "experiments", "detectors", "tags", "radioisotopes"];
+    default:
+      return ["experiments"];
+  }
+}
+
+const Nav = (props) => {
+  const { currentUserRole } = props;
+
   return (
     <nav>
       <ul className="list-none flex items-center gap-16">
-        {["users", "experiments", "detectors", "tags", "radioisotopes"].map(
-          (navlink, index) => {
-            return (
-              <li key={index}>
-                <NavLink
-                  to={`/${navlink}`}
-                  className={({ isActive }) => {
-                    return `capitalize font-medium outline-offset-4 ${
-                      isActive
-                        ? "font-bold text-sky-700 hover:text-sky-900 underline"
-                        : "hover:text-sky-700 hover:underline"
-                    }`;
-                  }}
-                >
-                  {navlink}
-                </NavLink>
-              </li>
-            );
-          }
-        )}
+        {navFoRole(currentUserRole).map((navlink, index) => {
+          return (
+            <li key={index}>
+              <NavLink
+                to={`/${navlink}`}
+                className={({ isActive }) => {
+                  return `capitalize font-medium outline-offset-4 ${
+                    isActive
+                      ? "font-bold text-sky-700 hover:text-sky-900 underline"
+                      : "hover:text-sky-700 hover:underline"
+                  }`;
+                }}
+              >
+                {navlink}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
